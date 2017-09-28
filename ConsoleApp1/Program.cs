1d7d1d408a5d119054e0dad3a5152b71ebb35c24
Vladislav Yaroshevich 550504 <unclesamserious2012@gmail.com>
2017-09-28 23:56:57 +0300

    Added main function

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Management;
+
+namespace HDSpecs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Properties props = new Properties();
+            props.runCommand();
+            props.printSpecs();
+        }
+    }
+}
